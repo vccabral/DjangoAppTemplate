4032558ba77abd35532a1b624372ecbe4a14e473
Victor Cabral <vccabral@gmail.com>
2012-11-09 15:58:30 -0500

    forms and urls

---
+++ template/app_name/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls.defaults import patterns, url
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic.simple import direct_to_template, redirect_to
+from django.contrib.auth.decorators import login_required, permission_required
+
