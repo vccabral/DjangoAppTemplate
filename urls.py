16ed797821cee80feb04f1f3ac9286798d28f974
Victor Cabral <vccabral@gmail.com>
2012-11-09 18:18:53 -0500

    template, url and view

--- urls.py
+++ urls.py
@@ -3,3 +3,6 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from django.views.generic.simple import direct_to_template, redirect_to
 from django.contrib.auth.decorators import login_required, permission_required
 
+urlpatterns = patterns('',
+                       url(r'^$', '{{ app_name }}.views.index', name='{{ app_name }}_index'),
+                       )
