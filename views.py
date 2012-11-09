3f05db7d84c4f105f4662c47fa49122935dda376
Victor Cabral <vccabral@gmail.com>
2012-11-09 18:37:40 -0500

    added missing import

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def index(request):
     return render_to_response('{{ app_name }}.html', locals(), context_instance=RequestContext(request))
